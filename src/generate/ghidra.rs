672bede0e43efac318b900d9514ba0b1a47a0800
PoweredByPie <poweredbypie@users.noreply.github.com>
2024-01-06 03:34:51 -0800

    Remove unused import in Java file

--- src/generate/ghidra.rs
+++ src/generate/ghidra.rs
@@ -5,6 +5,7 @@ use std::path::PathBuf;
 use crate::db::ExecDB;
 
 pub fn generate(json_path: PathBuf) -> Result<ExecDB, Box<dyn std::error::Error>> {
+    // The bulk of the work is done by the Ghidra script that emits a json file
     let reader = BufReader::new(File::open(json_path)?);
     let db = serde_json::from_reader(reader)?;
     Ok(db)
